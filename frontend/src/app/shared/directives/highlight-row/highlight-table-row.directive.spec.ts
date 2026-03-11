f32aa54e24a21fa6fb919848755892b382b13a0b
Bogdan <bogdan.bala@bmwtechworks.ro>
2026-03-11 15:12:57 +0200

    3 directives left

---
+++ frontend/src/app/shared/directives/highlight-row/highlight-table-row.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightTableRowDirective } from './highlight-table-row.directive';
+
+describe('HighlightTableRowDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightTableRowDirective();
+    expect(directive).toBeTruthy();
+  });
+});
